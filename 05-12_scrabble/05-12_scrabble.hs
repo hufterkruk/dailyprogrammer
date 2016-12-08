c1c3b20a4f18eaf4301ca6530122fa5eff34d0f1
Mommers,M. (Merijn) <m.mommers@students.uu.nl>
2016-12-08 12:43:39 +0100

    decided I didn't feel like implementing bonus 3 for 05-12_scrabble

--- 05-12_scrabble/05-12_scrabble.hs
+++ 05-12_scrabble/05-12_scrabble.hs
@@ -6,22 +6,9 @@ scrabble ys (x : xs)
   | x `elem` ys = scrabble (delete x ys) xs
   | '?' `elem` ys = scrabble (delete '?' ys) xs
   | otherwise = False
-
-letterScore :: Char -> Int
-letterScore x | x == '?'              = 0
-              | x `elem` "eaionrtlsu" = 1
-              | x `elem` "dg"         = 2
-              | x `elem` "bcmp"       = 3
-              | x `elem` "fhvwy"      = 4
-              | x == 'k'              = 5
-              | x `elem` "jx"         = 8
-              | otherwise             = 10
-
+  
 longest :: String -> IO String
 longest xs = last . filter (scrabble xs) <$> readWords
 
-deleteReturn :: String -> String
-deleteReturn xs = take (length xs - 1) xs
-
 readWords :: IO [String]
-readWords = (sortOn length . map deleteReturn) . lines <$> readFile "enable1.txt"
+readWords = (sortOn length . map (delete '\r')) . lines <$> readFile "enable1.txt"
