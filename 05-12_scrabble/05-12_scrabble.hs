5757e5b3db36381a941f37dccb028a5efc3bf2ae
Murray <merijnmom@gmail.com>
2016-12-09 14:06:57 +0100

    implemented last bonus as well

--- 05-12_scrabble/05-12_scrabble.hs
+++ 05-12_scrabble/05-12_scrabble.hs
@@ -6,9 +6,29 @@ scrabble ys (x : xs)
   | x `elem` ys = scrabble (delete x ys) xs
   | '?' `elem` ys = scrabble (delete '?' ys) xs
   | otherwise = False
-  
-longest :: String -> IO String
-longest xs = last . filter (scrabble xs) <$> readWords
 
-readWords :: IO [String]
-readWords = (sortOn length . map (delete '\r')) . lines <$> readFile "enable1.txt"
+letterScore :: Char -> Int
+letterScore x | x `elem` "eaionrtlsu" = 1
+              | x `elem` "dg"         = 2
+              | x `elem` "bcmp"       = 3
+              | x `elem` "fhvwy"      = 4
+              | x == 'k'              = 5
+              | x `elem` "jx"         = 8
+              | otherwise             = 10
+
+wordScore :: String -> String -> Int
+wordScore ys [] = -1 * (sum . map letterScore) ys
+wordScore ys (x:xs)
+  | not (scrabble ys (x:xs)) = 0
+  | x `elem` ys = letterScore x + wordScore (delete x ys) xs
+  | '?' `elem` ys = wordScore (delete x ys) xs
+  | otherwise = undefined
+
+highest :: String -> IO String
+highest xs = last . sortOn (wordScore xs) <$> readWords xs
+
+longest :: String -> IO String
+longest xs = last . sortOn length <$> readWords xs
+
+readWords :: String -> IO [String]
+readWords xs = filter (scrabble xs) . map (delete '\r') . lines <$> readFile "enable1.txt"
