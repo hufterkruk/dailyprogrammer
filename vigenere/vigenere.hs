9d0c0a160ca635adef5f6c84ff774dfa3cae6f42
Mommers,M. (Merijn) <m.mommers@students.uu.nl>
2017-02-23 14:59:47 +0100

    FUCK

--- vigenere/vigenere.hs
+++ vigenere/vigenere.hs
@@ -1,16 +1,26 @@
 import Data.Char (toLower)
 
+l2i :: Char -> Int
+l2i c = fromEnum c - 96
+
+i2l :: Int -> Char
+i2l i = toEnum (i+96) :: Char
+
+encrypt' :: String -> String -> String
+encrypt' [] _ = []
+encrypt' _ [] = error "no key!"
+encrypt' (m:ms) (k:ks) = enc m k : encrypt' ms ks
+  where
+    enc :: Char -> Char -> Char
+    enc a b = i2l $ (l2i a + l2i b) `mod` 26
+
 encrypt :: String -> String -> String
-ecrypt [] _ = []
-encrypt _ [] = error "doe ff nie"
-encrypt [m] [k] = toEnum (fromEnum toLower m + fromEnum toLower k)
-encrypt msg@(m:ms) key@(k:ks)
-  | length key < length msg
-    = encrypt msg (concat $ replicate ((ml `div` kl) + 1) key)
-  | otherwise
-    = toEnum (fromEnum m' + fromEnum k') : encrypt ms ks
+encrypt msg key
+  | kl < ml
+    = encrypt' lMsg (concat $ replicate ((ml `div` kl) + 1) lKey)
+  | otherwise = encrypt' lMsg lKey
   where
     kl = length key
     ml = length msg
-    m' = toLower m
-    k' = toLower k
+    lMsg = map toLower msg
+    lKey = map toLower key
